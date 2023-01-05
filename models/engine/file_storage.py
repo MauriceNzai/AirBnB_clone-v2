df83884012cbe8cbbf31ebe8adaa6eee5fe8df80
MauriceNzai <maurice.nzai@gmail.com>
2023-01-05 13:14:40 +0000

    Add public instance method delete() to file_stroage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        my_dict = {}
+        for key, val in FileStorage.__objects.items():
+            if isinstance(val, cls):
+                my_dict[key] = val
+        return my_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +54,18 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        deletes obj from __objects if it is inside, it does nothing if obj=None
+        """
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            del self.__objects[key]
+            self.save()
+
+    def close(self):
+        """
+        Calls method reload() for deserialising JSON file
+        """
+        self.reload()
