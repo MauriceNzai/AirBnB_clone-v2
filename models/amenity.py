b76c7386b1d7ee350fc57d01afe4c00348759ea8
MauriceNzai <maurice.nzai@gmail.com>
2023-01-10 11:46:55 +0000

    Changed

--- models/amenity.py
+++ models/amenity.py
@@ -6,11 +6,7 @@ from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
 
-<<<<<<< HEAD
-class Amenity(BaseModel, Base)
-=======
 class Amenity(BaseModel, Base):
->>>>>>> 9880d03592d7bf9187a679576411c793782e2575
     """
     Amenity class, Models an amenity
     """
