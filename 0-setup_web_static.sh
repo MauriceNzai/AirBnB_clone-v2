7e62a53e6fe92e46362faa4e49edab239a395052
MauriceNzai <maurice.nzai@gmail.com>
2023-01-23 21:36:07 +0000

    Script to set up web servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# This scripts sets up my web servers for the deployment of web_static
+# Sets up web servers for the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
