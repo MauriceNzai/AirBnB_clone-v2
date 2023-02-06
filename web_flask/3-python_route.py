5d7dbb2ed32ef21e01143c9b159f0ca7775dc2c9
MauriceNzai <maurice.nzai@gmail.com>
2023-02-06 13:09:44 +0000

    check style

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -37,5 +37,5 @@ def python_text(text='is_cool'):
     return ("Python {}".format(text.replace('_', ' ')))
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
