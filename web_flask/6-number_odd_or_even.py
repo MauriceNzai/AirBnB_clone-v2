44996c8923047d1896d2c0ea986fa32a78a83fa9
MauriceNzai <maurice.nzai@gmail.com>
2023-02-06 13:16:59 +0000

    updated

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -14,12 +14,14 @@ def hello():
     """
     return ("Hello HBNB!")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Adds the route /hbnb
     """
     return ("HBNB")
+
 
 @app.route('/c/<string:text>', strict_slashes=False)
 def dynamic_text(text=None):
@@ -28,6 +30,7 @@ def dynamic_text(text=None):
     """
     return ("C {}".format(text.replace('_', ' ')))
 
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python_text(text='is_cool'):
@@ -35,6 +38,7 @@ def python_text(text='is_cool'):
     Display dynamic text, and replace underscore for space
     """
     return ("Python {}".format(text.replace('_', ' ')))
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def dynamic_digits(n=None):
@@ -50,6 +54,7 @@ def number_template(n=None):
     Display a HTML page only if input number, n is an integer
     """
     return (render_template('5-number.html', n=n))
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def conditional_template(n=None):
