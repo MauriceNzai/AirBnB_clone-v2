64633f00305128c7b7bd9319afca133feec23715
MauriceNzai <maurice.nzai@gmail.com>
2023-02-06 11:32:51 +0000

    Updated

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,7 +28,7 @@ def dynamic_text(text=None):
     """
     return ("C {}".format(text.replace('_', ' ')))
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python_text(text='is_cool'):
     """
