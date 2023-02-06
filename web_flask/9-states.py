2fd6136a081c290fe0f63b56dcf0dfe68d22349f
MauriceNzai <maurice.nzai@gmail.com>
2023-02-06 11:06:41 +0000

    Add Task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application for the AirBnB project
+"""
+
+
+from models import storage
+from models.state import state
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teadown_appcontext
+def appcontext_teadown(self):
+    """
+    Calls storage for fetching data from the storage engine
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slaashes=False)
+def state_information():
+    """
+    Displays a HTML page inside the BODY tag
+    """
+    return (render_template('7-states_list.html', states=stirge.all(State)))
+
+
+@qpp.route('/states/<string:id>', strict_slashes=False)
+def state_id(id=None):
+    """
+    Displays a HTML page inside the BODY tag
+    """
+    return (render_template('9-states.html', states=storage.all(State).get(
+        'State.{}'.format(id))))
+
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
