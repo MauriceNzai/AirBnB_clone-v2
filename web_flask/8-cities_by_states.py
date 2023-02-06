c7f3ce9fcce66ff1723dec005ea114d41b32ff30
MauriceNzai <maurice.nzai@gmail.com>
2023-02-06 12:15:38 +0000

    Updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@ Starts a Flask web application for the HBNB project
 from models import storage
 from models.state import State
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -18,8 +19,8 @@ def appcontext_teardown(self):
 @app.route('/cities_by_states', strict_slashes=False)
 def state_information():
     """Display a HTML page inside the tag BODY"""
-    return render_template('8-cities_by_states.html',
-                            states=storage.all(State))
+    return (render_template('8-cities_by_states.html',
+                            states=storage.all(State)))
 
 
 if __name__ == '__main__':
