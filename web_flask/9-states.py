6d16403bfe26cee3aef1c256e5954ed04a01b660
MauriceNzai <maurice.nzai@gmail.com>
2023-02-07 08:58:52 +0000

    Updating

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,15 +6,16 @@ Starts a Flask web application for the AirBnB project
 
 from models import storage
 from models.state import State
+from models.city import City
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def appcontext_teardown(self):
+def appcontext_teardown(exc):
     """
-    Calls storage for fetching data from the storage engine
+    Removes current SQLAlchemy session
     """
     storage.close()
 
@@ -24,7 +25,7 @@ def state_information():
     """
     Displays a HTML page inside the BODY tag
     """
-    return (render_template('7-states_list.html', states=storage.all(State)))
+    return (render_template('9-states.html', states=storage.all(State)))
 
 
 @app.route('/states/<string:id>', strict_slashes=False)
@@ -32,9 +33,10 @@ def state_id(id=None):
     """
     Displays a HTML page inside the BODY tag
     """
-    return (render_template('9-states.html',
-            states=storage.all(State).get('State.{}'.format(id))))
-
+    for state in storage.all('State').values():
+        if state.id == id:
+            return (render_template('9-states.html', state = state))
+        return (render_template('9-states.html'))
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
