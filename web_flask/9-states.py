f9b4a53650d0248de69ff9abbb0f768aa10427a6
MauriceNzai <maurice.nzai@gmail.com>
2023-02-12 12:23:10 +0000

    Checking doc style

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application for task 9
-and checks the status of the app
+Starts a Flask Web applictation for the project
+Created 2023
 """
 from models import storage
 from models.state import State
-from models.city import City
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def appcontext_teardown(self):
-    """
-    Removes current SQLAlchemy session
+    """use storage for fetching data from the storage engine
     """
     storage.close()
 
 
 @app.route('/states', strict_slashes=False)
 def state_info():
-    """
-    Display HTML Page inside Body tag
-    """
-    return (render_template('7-states_list.html', states=storage.all(State)))
+    """Display a HTML page inside the tag BODY"""
+    return render_template('7-states_list.html',
+                           states=storage.all(State))
 
 
 @app.route('/states/<string:id>', strict_slashes=False)
 def state_id(id=None):
-    """
-    Displays a HTML page inside the BODY tag
-    """
-    return (render_template('9-states.html', states=storage.all(State).get(
-            'State.{}'.format(id))))
+    """Display a HTML page inside the tag BODY"""
+    return render_template('9-states.html',
+                           states=storage.all(State)
+                           .get('State.{}'.format(id)))
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
