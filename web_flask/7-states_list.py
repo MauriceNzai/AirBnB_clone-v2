e0afb843c0e7feff81c98535a04c6f1651ef146e
MauriceNzai <maurice.nzai@gmail.com>
2023-02-13 10:56:02 +0000

    Updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
-from flask import Flask, render_template
+"""
+Starts a Flask web apllication for the HBNB project
+"""
+
 from models import storage
 from models.state import State
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def appcontext_teardown(self):
+    """
+    Uses storage for fetching data from storage engine
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States",
-                           items=dict_to_html)
+def state_information():
+    """
+    Display a HTML page inside tag BODY
+    """
+    return (render_template('7-states_list.html', states=storage.all(State)))
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
